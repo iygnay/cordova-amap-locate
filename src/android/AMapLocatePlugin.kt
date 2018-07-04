5fd7f491ef05d961f753652ba990e295b84c66e9
kain <zhangtqx@vip.qq.com>
2018-07-04 15:32:30 +0800

    Update

--- src/android/AMapLocatePlugin.kt
+++ src/android/AMapLocatePlugin.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.ActivityCompat
 import com.amap.api.location.AMapLocation
 import com.amap.api.location.AMapLocationClient
 import com.amap.api.location.AMapLocationClientOption
+import com.amap.api.location.AMapLocationClientOption.AMapLocationMode
 import com.amap.api.location.AMapLocationListener
 import io.reactivex.subjects.AsyncSubject
 import org.apache.cordova.CallbackContext
@@ -14,8 +15,6 @@ import org.json.JSONArray
 
 class AMapLocatePlugin : CordovaPlugin(), AMapLocationListener {
     private var mLocationClient: AMapLocationClient? = null
-    private var mLocationClientOption: AMapLocationClientOption? = null
-
     private val asyncSubject: AsyncSubject<AMapLocation> = AsyncSubject.create()
     private val PERMISSON_REQUESTCODE: Int = 0;
     private val checkPermission: Array<String> = arrayOf(
@@ -33,25 +32,6 @@ class AMapLocatePlugin : CordovaPlugin(), AMapLocationListener {
         initAMapLocation()
     }
 
-    override fun execute(action: String?, args: JSONArray?, callbackContext: CallbackContext?): Boolean {
-        initAMapLocation()
-        when (action) {
-            "getCurrentPosition" -> {
-                getCurrentPosition(callbackContext!!)
-                return true
-            }
-            else -> return false
-        }
-    }
-
-    private fun initAMapLocation() {
-        mLocationClientOption = AMapLocationClientOption()
-        mLocationClientOption!!.setOnceLocation(true)
-        mLocationClient = AMapLocationClient(cordova.activity.application.applicationContext)
-        mLocationClient!!.setLocationOption(mLocationClientOption);
-        mLocationClient!!.setLocationListener(this);
-    }
-
     private fun checkPermissions() {
         ActivityCompat.requestPermissions(
                 cordova.activity,
@@ -60,21 +40,41 @@ class AMapLocatePlugin : CordovaPlugin(), AMapLocationListener {
         )
     }
 
+    private fun initAMapLocation() {
+        val mLocationClientOption = AMapLocationClientOption()
+        mLocationClientOption.setOnceLocation(true)
+        mLocationClientOption.setLocationMode(AMapLocationMode.Hight_Accuracy)
+
+        mLocationClient = AMapLocationClient(cordova.activity.application.applicationContext)
+        mLocationClient!!.setLocationOption(mLocationClientOption);
+        mLocationClient!!.setLocationListener(this);
+    }
+
+    override fun execute(action: String?, args: JSONArray?, callbackContext: CallbackContext?): Boolean {
+        if (action == "getCurrentPosition") {
+            getCurrentPosition(callbackContext!!)
+            return true
+        } else return false
+    }
+
+    private fun getCurrentPosition(callbackContext: CallbackContext) {
+        mLocationClient!!.startLocation()
+        val json = JSONArray()
+        asyncSubject.subscribe({
+            mLocationClient!!.stopLocation()
+            json.put(it.getLatitude())
+            json.put(it.getLongitude())
+            callbackContext.success(json)
+        }, {
+            mLocationClient!!.stopLocation()
+            callbackContext.error(0)
+        })
+    }
+
     override fun onLocationChanged(location: AMapLocation?) {
         if (location?.errorCode == 0) {
             asyncSubject.onNext(location)
             asyncSubject.onComplete()
         }
-    }
-
-    fun getCurrentPosition(callbackContext: CallbackContext) {
-        mLocationClient!!.startLocation()
-        asyncSubject.subscribe({
-            var json = JSONArray()
-            json.put(it.getLatitude())
-            json.put(it.getLongitude())
-            callbackContext.success(json)
-            mLocationClient!!.stopLocation()
-        })
     }
 }
